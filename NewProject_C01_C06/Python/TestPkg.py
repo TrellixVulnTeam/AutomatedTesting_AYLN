813e13962429cc5b3f67e50aa756e47576020a4a
Lizhigen <lizhigen1996@aliyun.com>
2021-10-17 15:55:46 +0800

    更新Python包，使其能压缩7z文件，但7z压缩时间太长，目前默认压缩zip格式

--- NewProject_C01_C06/Python/TestPkg.py
+++ NewProject_C01_C06/Python/TestPkg.py
@@ -12,15 +12,16 @@ import sys
 import tarfile
 import shutil
 import zipfile
+import py7zr
 from datetime import datetime
 
 
-def creatPakage(path: str, isRemove=False, mode=True):
+def creatPakage(path: str, isRemove=False, mode=2):
     '''
     打包测试文件夹
     :param path: 文件夹路径
     :param isRemove: 打包后是否删除源文件夹
-    :param mode: 保存为zip或tar.gz
+    :param mode: 保存为zip或tar.gz或7z(0,1,2)
     :return:
     '''
     if not os.path.exists(path):
@@ -29,41 +30,51 @@ def creatPakage(path: str, isRemove=False, mode=True):
     if os.path.isfile(path):
         return'Sorry, This function can only compress folders.'
 
-    if mode:
-        p_pakage = path + datetime.now().strftime('_%Y%m%d%H%M%S') + '.zip'
+    _t = datetime.now().strftime('_%Y%m%d%H%M%S')
+    os.rename(path, path + _t)
+    path += _t
+
+    if mode == 0:
+        p_pakage = path + '.zip'
         if os.path.exists(p_pakage):
             os.remove(p_pakage)
         zf = zipfile.ZipFile(p_pakage, 'w')
-        recursion(zf, path)
+        recursion(zf, path, mode=mode)
         zf.close()
-    else:
-        p_pakage = path + datetime.now().strftime('_%Y%m%d%H%M%S') + '.tar.gz'
+    elif mode == 1:
+        p_pakage = path + '.tar.gz'
         if os.path.exists(p_pakage):
             os.remove(p_pakage)
         tar = tarfile.open(p_pakage, "w:gz")
         tar.add(path)
         tar.close()
+    elif mode == 2:
+        p_pakage = path + '.7z'
+        if os.path.exists(p_pakage):
+            os.remove(p_pakage)
+        archive = py7zr.SevenZipFile(p_pakage, 'w')
+        recursion(archive, path, mode=mode)
+        archive.close()
+    else:
+        raise Exception('Mode code error.')
 
     if isRemove:
         shutil.rmtree(path)
 
 
-def recursion(fp: zipfile.ZipFile, folderPath, currParh=None):
+def recursion(fp, folderPath, currParh=None, mode=2):
     if currParh is None:
         currParh = folderPath
     files = os.listdir(currParh)
     for file in files:
         filePath = os.path.join(currParh, file)
         if os.path.isfile(filePath):
-            fp.write(
-                filePath,
-                arcname=os.path.join(
-                    filePath.replace(
-                        folderPath,
-                        '')),
-                compress_type=zipfile.ZIP_DEFLATED)
+            if mode == 0:
+                fp.write(filePath, arcname=os.path.join(filePath.replace(folderPath, '')), compress_type=zipfile.ZIP_DEFLATED)
+            elif mode == 2:
+                fp.write(filePath, arcname=os.path.join(filePath.replace(folderPath, '')))
         elif os.path.isdir(filePath):
-            recursion(fp, folderPath, filePath)
+            recursion(fp, folderPath, filePath, mode)
 
 
 def deleteFiles(folderPath):
@@ -111,7 +122,7 @@ if __name__ == '__main__':
     parser = argparse.ArgumentParser(prog="package")
     parser.add_argument('-z', '--zipPath', help="需要压缩的文件夹路径", required=True, type=str)
     parser.add_argument('-r', '--remove', help="压缩后是否删除原文件夹", type=bool, default=True, choices=[True, False])
-    parser.add_argument('-m', '--mode', help="压缩文件的格式", type=bool, default=True, choices=[True, False])
+    parser.add_argument('-m', '--mode', help="压缩文件的格式", type=int, default=0, choices=[0, 1, 2])
     parser.add_argument('-d', '--deletePath', help="需要监控删除的文件夹", default='', type=str)
     args = parser.parse_args()
 
